8e16fd4d9f0c92274f243d73c1335d348ba0b078
MohammadShahansha <shahansha.cou.ict11@gmail.com>
2023-12-15 01:33:23 +0600

    added course interface, model, zodValidation

---
+++ src/app/modules/course/course.model.ts
@@ -0,0 +1,68 @@
+import { Schema, model } from 'mongoose';
+import { TCourse } from './course.interface';
+
+const CourseSchema = new Schema<TCourse>({
+  title: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  instructor: {
+    type: String,
+    required: true,
+  },
+  categoryId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  //   tags: [
+  //     {
+  //       name: {
+  //         type: String,
+  //         required: true,
+  //       },
+  //       isDeleted: {
+  //         type: Boolean,
+  //         default: false,
+  //       },
+  //     },
+  //   ],
+  startDate: {
+    type: String,
+    required: true,
+  },
+  endDate: {
+    type: String,
+    required: true,
+  },
+  language: {
+    type: String,
+    required: true,
+  },
+  provider: {
+    type: String,
+    required: true,
+  },
+  durationInWeeks: {
+    type: Number,
+    required: true,
+  },
+  details: {
+    level: {
+      type: String,
+      enum: ['Beginner', 'Intermediate', 'Advanced'],
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+});
+
+export const Course = model<TCourse>('Course', CourseSchema);
---
+++ src/app/modules/course/course.zod.validation.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod';
+
+export const courseValidation = z.object({
+  title: z.string(),
+  instructor: z.string(),
+  //   categoryId: z.string(),
+  price: z.number(),
+  tags: z.array(
+    z.object({
+      name: z.string(),
+      isDeleted: z.boolean(),
+    }),
+  ),
+  startDate: z.string(),
+  endDate: z.string(),
+  language: z.string(),
+  provider: z.string(),
+  durationInWeeks: z.number(),
+  details: z.object({
+    level: z.enum(['Beginner', 'Intermediate', 'Advanced']),
+    description: z.string(),
+  }),
+});
